1ea4240f6785104c497752df7c3ff52c08534a30
二葉琉璃 a.k.a ワールドキュート <153579552+SekaiMoe@users.noreply.github.com>
2026-04-30 20:55:05 +0800

    Refactor build script for multi-architecture binaries

--- build_multiarch.sh
+++ build_multiarch.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-targets=("linux/amd64" "linux/arm64" "darwin/amd64" "windows/amd64")
-for t in "${targets[@]}"; do
-    IFS='/' read -r goos goarch <<< "$t"
-    GOOS=$goos GOARCH=$goarch go build -o bin/asikad-$goos-$goarch ./cmd/asikad &
+bins=("asika" "asikad")
+platforms=("linux/amd64" "linux/arm64" "darwin/amd64" "windows/amd64")
+
+for bin in "${bins[@]}"; do
+    for platform in "${platforms[@]}"; do
+        GOOS=${platform%/*} GOARCH=${platform#*/} \
+        go build -ldflags="-s -w" -o "bin/${bin}-${platform//\//-}" ./cmd/${bin} &
+    done
 done
 wait
