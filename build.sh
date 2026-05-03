dfcf60b66d8f8f18a27f69c871163a05a61ebc71
root <root@localhost>
2026-05-03 18:58:46 +0800

    Move service/* to pack repo

--- build.sh
+++ build.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
 
-env go build -o asikad ./cmd/asikad/main.go
-env go build -o asika ./cmd/asika/main.go
+error() {
+	echo "Error: $1" >&2
+	exit 1
+}
+
+SU() {
+	if [ "$(id -u)" -eq 0 ]; then
+		"$@"
+	else
+		sudo "$@"
+	fi
+}
+
+build() {
+	env go build -o asikad ./cmd/asikad/main.go
+	env go build -o asika ./cmd/asika/main.go
+}
+
+clean() {
+	rm -rf asika* asikad*
+}
+
+distclean() {
+	clean
+	rm -rf ~/.cache/go-build
+	SU rm -rf ~/go
+}
+
+build
